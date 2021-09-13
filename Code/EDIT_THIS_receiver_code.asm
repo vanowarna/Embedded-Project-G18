a6b13140292f5db71ae886f329baeacb5467c809
vanowarna <55307326+vanowarna@users.noreply.github.com>
2021-09-13 22:54:41 +0530

    Edit receiver added
    Signed-off-by: vanowarna <55307326+vanowarna@users.noreply.github.com>

---
+++ Code/EDIT_THIS_receiver_code.asm
@@ -0,0 +1,71 @@
+  LIST p=16F84A
+  #INCLUDE <p16f84a.inc>
+
+  ORG 0x00
+   
+; SENSOR    RA1     INPUT       TRISA   B'00000010'
+; BIT0      RB1     OUTPUT      TRISB   B'00000000'
+; BIT1      RB2     OUTPUT
+; BIT2      RB3     OUTPUT
+; BIT3      RB4     OUTPUT
+
+   CBLOCK 0CH
+    COUNT1
+    COUNT2
+    COUNT3
+    STORE_INP
+   ENDC
+
+;Define Registers
+MAIN
+   BSF   STATUS,5
+   CLRF  TRISB
+   MOVLW    B'00000010'
+   MOVWF    TRISA
+   BCF   STATUS,5
+   CLRF  PORTB
+   CLRF  PORTA
+   CLRF  STORE_INP
+
+INPUT_CHECK
+    BTFSS PORTA,2
+    GOTO INPUT_CHECK
+    GOTO READ_INPUT
+
+READ_INPUT
+      CALL  DB_DELAY
+   ; =================
+   BTFSC PORTA,1
+   BSF   STORE_INP,0
+   BTFSC PORTA,1
+   BSF   STORE_INP,1
+   BTFSC PORTA,1
+   BSF   STORE_INP,2
+   BTFSC PORTA,1
+   BSF   STORE_INP,3
+   ; =================
+   
+DELAY
+   DECFSZ  COUNT2,1
+   GOTO    DELAY
+   DECFSZ  COUNT3,1
+   GOTO    DELAY
+   RETURN
+
+OUTPUT
+   MOVF STORE_INP,0
+   MOWF PORTB
+END   
+
+
+
+
+
+
+
+
+
+
+
+
+
