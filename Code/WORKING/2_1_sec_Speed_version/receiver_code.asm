d118c34715f4e02ebae4008dbaf3927a0fd6ae95
vanowarna <55307326+vanowarna@users.noreply.github.com>
2021-09-23 13:57:41 +0530

    Stable code
    Signed-off-by: vanowarna <55307326+vanowarna@users.noreply.github.com>

--- Code/WORKING/2_1_sec_Speed_version/receiver_code.asm
+++ Code/WORKING/2_1_sec_Speed_version/receiver_code.asm
@@ -1,101 +1,80 @@
-  LIST p=16F84A
+LIST p=16F84A
   #INCLUDE <p16f84a.inc>
 
-  ORG 0x00
-   
+  ORG 0x00		    ; Configure Origin vector
+
+; PIN CONFIGURATION
+; ---------------------------
 ; SENSOR    RA1     INPUT       TRISA   B'00000010'
 ; BIT0      RB1     OUTPUT      TRISB   B'00000000'
 ; BIT1      RB2     OUTPUT
 ; BIT2      RB3     OUTPUT
 ; BIT3      RB4     OUTPUT
-;  000xxxx0
+; ---------------------------
 
-   CBLOCK 0CH
+   CBLOCK 0CH		    ; Reserving General registers
     COUNT1
     COUNT2
     COUNT3
     STORE_INP
    ENDC
 
-;Define Registers
-MAIN
-   BSF   STATUS,5
-   CLRF  TRISB
-   MOVLW    B'00000010'
+MAIN			    ; Configuring registers    
+   BSF   STATUS,5	    ; Moving to Bank 1
+   CLRF  TRISB		    ; Set PORTB as Output
+   MOVLW    B'00000010'	    ; Input Pin configuration in PORTA
    MOVWF    TRISA
-   BCF   STATUS,5
-   CLRF  PORTB
-   CLRF  PORTA
-   CLRF  STORE_INP
+   BCF   STATUS,5	    ; Moving back to Bank 0
+   CLRF  PORTB		    ; Set PORTB to LOW (B'00000000')
+   CLRF  PORTA		    ; Set PORTA to LOW (B'00000000')
+   CLRF  STORE_INP	    ; Clearing stored input
 
-INPUT_CHECK
-    BTFSS PORTA,1
-    GOTO INPUT_CHECK
-    GOTO READ_INPUT
+INPUT_CHECK		    
+    BTFSS PORTA,1	    ; Check RA1 for Inputs, skip if HIGH
+    GOTO INPUT_CHECK	    ; Loop until RA1, HIGH
+    GOTO READ_INPUT	    
 
 READ_INPUT
-
-;      CALL  DB_DELAY
-   ; =================
-;   BTFSC PORTA,1
-;   BSF   STORE_INP,0
-;   BTFSC PORTA,1
-;   BSF   STORE_INP,1
-;   BTFSC PORTA,1
-;   BSF   STORE_INP,2
-;   BTFSC PORTA,1
-;   BSF   STORE_INP,3
-   ; =================
-   CALL DELAY
-   CALL DELAY
+   CALL DELAY		    ; Calling DELAY 3 times to get to the middle...
+   CALL DELAY		    ; ... of the Required Laser pulse...
+   CALL DELAY		    ; ... This avoids the Dummy pulse
+   
+   BTFSC PORTA,1	    ; Check Pin RA1, skip if LOW
+   BSF   STORE_INP,1	    ; Else, add HIGH to bit 1 in STORE_INP
+   
+   CALL DELAY		    ; Calling DELAY 4 times to get to the middle...
+   CALL DELAY		    ; ... of the Required Laser pulse...
+   CALL DELAY		    ; ... This avoids the Dummy pulse 
    CALL DELAY
    
-   BTFSC PORTA,1
-   BSF   STORE_INP,1
+   BTFSC PORTA,1	    ; Check Pin RA1, skip if LOW
+   BSF   STORE_INP,2	    ; Else, add HIGH to bit 2 in STORE_INP
    
    CALL DELAY
    CALL DELAY
    CALL DELAY
    CALL DELAY
    
-   BTFSC PORTA,1
-   BSF   STORE_INP,2
-   
-   CALL DELAY
-   CALL DELAY
-   CALL DELAY
-   CALL DELAY
-   
-   BTFSC PORTA,1
-   BSF   STORE_INP,3
+   BTFSC PORTA,1	    ; Check Pin RA1, skip if LOW
+   BSF   STORE_INP,3	    ; Else, add HIGH to bit 3 in STORE_INP
 
    CALL DELAY
    CALL DELAY
    CALL DELAY
    CALL DELAY
    
-   BTFSC PORTA,1
-   BSF   STORE_INP,4
-  
+   BTFSC PORTA,1	    ; Check Pin RA1, skip if LOW
+   BSF   STORE_INP,4	    ; Else, add HIGH to bit 4 in STORE_INP
    
 OUTPUT
-   CLRF  PORTB
-   MOVF STORE_INP,0
-   MOVWF PORTB
+   CLRF  PORTB		    ; Set PORTB to LOW (B'00000000')
+   MOVF  STORE_INP,0	    ; Move STORE_INP to W Register
+   MOVWF PORTB		    ; Move W Register to PORTB
 
-   CLRF STORE_INP
+   CLRF STORE_INP	    ; Clearing stored input
    GOTO INPUT_CHECK   
 
-; DELAY
-;    DECFSZ  COUNT2,1
-;    GOTO    DELAY
-;    DECFSZ  COUNT3,1
-;    GOTO    DELAY
-;    RETURN
-
-; Added============ 1 second ========================
-DELAY
-			;1108399 cycles
+DELAY			    ; 1 Second delay
 	MOVLW	0x86
 	MOVWF	COUNT1
 	MOVLW	0x6B
@@ -110,11 +89,8 @@ DELAY_0
 	DECFSZ	COUNT3, f
 	GOTO	DELAY_0
 
-			;2 cycles
 	GOTO	$+1
 
-			;4 cycles (including call)
-	RETURN
-; Added====================================  
+	RETURN 
  
 END
--- Code/WORKING/2_1_sec_Speed_version/transmitter_code.asm
+++ Code/WORKING/2_1_sec_Speed_version/transmitter_code.asm
@@ -1,69 +1,75 @@
 LIST p=16F84A
   #INCLUDE <p16f84a.inc>
 
-  ORG 0x00
+  ORG 0x00		; Configure Origin vector
   GOTO MAIN
-  ORG 0x04
-  GOTO INT
-
+  ORG 0x04		; Configure Interrupt vector
+  GOTO INT  
+  
+; PIN CONFIGURATION
+; ---------------------------
 ; LASER    RA0     OUTPUT       TRISA   B'00000000'
 ; SEND     RB0     INTERRUPT    TRISB   B'00011111'
 ; BIT0     RB1     INPUT
 ; BIT1     RB2     INPUT
 ; BIT2     RB3     INPUT
 ; BIT3     RB4     INPUT
+; ---------------------------
 
-   CBLOCK 0CH
+   CBLOCK 0CH		; Reserving General registers
     COUNT1
     COUNT2
     COUNT3
     STORE_INP
    ENDC
 
-MAIN
-   BSF   STATUS,5
-   CLRF  TRISA
-   MOVLW B'00011111'
+MAIN			; Configuring registers
+   BSF   STATUS,5	; Moving to Bank 1
+   CLRF  TRISA		; Set PORTA as Output
+   MOVLW B'00011111'	; Input Pin configuration in PORTB
    MOVWF TRISB
-   BCF   STATUS,5
-   CLRF  PORTA
-INTDEF
+   BCF   STATUS,5	; Moving back to Bank 0
+   CLRF  PORTA		; Set PORTA to LOW (B'00000000')
+
+INTDEF			; Configuring interrupt
    MOVLW B'10010000'
    MOVWF INTCON
 
 LOOP
-   GOTO LOOP    ;Loop until interupt happens
+   GOTO LOOP		; Loop until interupt happens
 
 INT
-   CALL  DB_DELAY
-   ; =================
-   CLRF  STORE_INP
-   BTFSC PORTB,1
-   BSF   STORE_INP,0
-   BTFSC PORTB,2
-   BSF   STORE_INP,1
-   BTFSC PORTB,3
-   BSF   STORE_INP,2
-   BTFSC PORTB,4
-   BSF   STORE_INP,3
-   ; =================
-   CALL  LASER_PULSE
-   CLRF  STORE_INP
-   BCF	INTCON,INTF		
-   RETFIE			
+   CALL  DB_DELAY	; Calling Debounce delay
+   ; -------------------
+   CLRF  STORE_INP	; Clearing stored input
+   BTFSC PORTB,1	; Check RB1, skip if LOW
+   BSF   STORE_INP,0	; Else, add LOW to bit 0 in STORE_INP
+   BTFSC PORTB,2	; Check RB2, skip if LOW
+   BSF   STORE_INP,1	; Else, add LOW to bit 1 in STORE_INP
+   BTFSC PORTB,3	; Check RB3, skip if LOW
+   BSF   STORE_INP,2	; Else, add LOW to bit 2 in STORE_INP
+   BTFSC PORTB,4	; Check RB4, skip if LOW
+   BSF   STORE_INP,3	; Else, add LOW to bit 2 in STORE_INP
+   ; -------------------
+   CALL  LASER_PULSE	
+   CLRF  STORE_INP	; Clearing stored input
+   BCF	INTCON,INTF	; Clearing External Interrupt Flag bit
+   RETFIE		; Return from the Interrupt loop	
     
-DB_DELAY
+DB_DELAY		; Debounce delay
    DECFSZ  COUNT1,1
    GOTO    DB_DELAY 
    RETURN
-
-;  BIT3,X,BIT2,X,BIT1,X,BIT0,X
-LASER_PULSE
-   ; DUMMY PULSE 
+   
+; -------------------
+; DUMMY PULSE X = 1
+; [BIT3 X BIT2 X BIT1 X BIT0 X]
+LASER_PULSE		; Sending the Pulse signal to RA0
+   ; DUMMY PULSE
    BSF   PORTA,0
    CALL  DELAY
    BCF   PORTA,0
-   ;BIT0 
+   ; BIT0 
    BTFSC STORE_INP,0
    BSF   PORTA,0
    CALL  DELAY
@@ -71,7 +77,7 @@ LASER_PULSE
    BSF   PORTA,0
    CALL  DELAY
    BCF   PORTA,0
-   ;BIT1 
+   ; BIT1 
    BTFSC STORE_INP,1
    BSF   PORTA,0
    CALL  DELAY
@@ -79,7 +85,7 @@ LASER_PULSE
    BSF   PORTA,0
    CALL  DELAY
    BCF   PORTA,0
-   ;BIT2 
+   ; BIT2 
    BTFSC STORE_INP,2
    BSF   PORTA,0
    CALL  DELAY
@@ -87,23 +93,15 @@ LASER_PULSE
    BSF   PORTA,0
    CALL  DELAY
    BCF   PORTA,0
-   ;BIT3 
+   ; BIT3 
    BTFSC STORE_INP,3
    BSF   PORTA,0
    CALL  DELAY
    BCF   PORTA,0
    RETURN
+; -------------------
 
-; DELAY
-;    DECFSZ  COUNT2,1
-;    GOTO    DELAY
-;    DECFSZ  COUNT3,1
-;    GOTO    DELAY
-;    RETURN
-
-; Added=========2 second delay=====================
-DELAY
-			;2216800 cycles
+DELAY			; 2 Second delay
 	MOVLW	0x0D
 	MOVWF	COUNT1
 	MOVLW	0xD6
@@ -117,14 +115,9 @@ DELAY_0
 	GOTO	$+2
 	DECFSZ	COUNT3, f
 	GOTO	DELAY_0
-
-			;6 cycles
 	GOTO	$+1
 	GOTO	$+1
 	GOTO	$+1
-
-			;4 cycles (including call)
 	RETURN
-; Added====================================   
 
 END
